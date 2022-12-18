b23a1d78a9321eae16e00026bbe6fe5ed224ce4f
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-12-18 20:32:36 -0300

    update, falta so preferencia do audio agr

--- app/src/main/java/com/example/whack_a_mole_02/GameActivity.java
+++ app/src/main/java/com/example/whack_a_mole_02/GameActivity.java
@@ -2,6 +2,9 @@ package com.example.whack_a_mole_02;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -26,6 +29,11 @@ public class GameActivity extends AppCompatActivity implements Runnable {
     Chronometer chronometer;
     TextView txtScore;
     Boolean isSaved = false;
+    MediaPlayer mp;
+    SoundPool soundPool;
+    AudioAttributes audioAttributes;
+    int maxStreams;
+    int snd_score, snd_hammer;
 
     final int DELAY_START = 500;
     final int DELAY_MOLE_UP = 800;
@@ -52,13 +60,30 @@ public class GameActivity extends AppCompatActivity implements Runnable {
         btnMoles[6] = findViewById(R.id.btnMole07);
         btnMoles[7] = findViewById(R.id.btnMole08);
         btnMoles[8] = findViewById(R.id.btnMole09);
+
+        audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+
+        maxStreams = 2;
+
         isSaved = false;
+        mp = MediaPlayer.create(this, R.raw.music);
+        soundPool = new SoundPool.Builder()
+                .setAudioAttributes(audioAttributes)
+                .setMaxStreams(maxStreams)
+                .build();
+
+        snd_score = soundPool.load(this, R.raw.mole_hit, 1);
+        snd_hammer = soundPool.load(this, R.raw.martelada, 1);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onStart() {
+        super.onStart();
         handler.postDelayed(this, DELAY_START);
+        mp.start();
         chronometer.start();
     }
 
@@ -71,23 +96,26 @@ public class GameActivity extends AppCompatActivity implements Runnable {
             MoleShow(ChooseRandomMole());
         }
         else {
-
             if(!isSaved) {
                 saveScore();
                 isSaved = true;
             }
-
             chronometer.stop();
         }
-
         handler.postDelayed(this, DELAY_BETWEEN_MOLE);
     }
 
     public void onClick(View view){
         if(view == btnFinish){
+            mp.stop();
             Intent intent = new Intent(this, EndActivity.class);
             startActivity(intent);
         }
+    }
+
+    public void onClickMole(View view)
+    {
+        soundPool.play(snd_hammer, 100, 100, 1, 0, 1);
     }
 
     private int ChooseRandomMole(){
@@ -105,11 +133,15 @@ public class GameActivity extends AppCompatActivity implements Runnable {
 
     private void MoleShow(int randomMole){
         btnMoles[randomMole].setBackground(getDrawable(R.drawable.selector_btn_mole));
+        btnMoles[randomMole].setForeground(getDrawable(R.drawable.selector_btn_mole));
 
         handler.postDelayed(() -> {
-            if(btnMoles[randomMole].isPressed())
+            if(btnMoles[randomMole].isPressed()) {
                 score++;
+                soundPool.play(snd_score, 100, 100, 1, 0, 1);
+            }
             btnMoles[randomMole].setBackground(getDrawable(R.drawable.mole_down));
+            btnMoles[randomMole].setForeground(getDrawable(R.drawable.mole_down));
         }, DELAY_MOLE_UP);
     }
 
